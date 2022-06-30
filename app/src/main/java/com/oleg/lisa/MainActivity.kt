a85b10d4c32f52332c8027c81c340e2761a12e7c
Lukmannudin <lukmannudin.priatna@happyfresh.com>
2022-07-01 01:57:39 +0700

    Initial commit

---
+++ app/src/main/java/com/oleg/lisa/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.oleg.lisa
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        startActivity(Intent(this, ChatRoomActivity::class.java))
+    }
+}
